47f372b784cefd2994d797cb64ffac9a5f973f8c
win2flash <win2flash@gmail.com>
2015-03-19 23:49:17 +0300

    ...

--- alarm.py
+++ alarm.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
 import datetime
-import pyglet
+import pygame
 root = Tk()
 
-def Time(event):
+def time(event):
     lbl["text"] = datetime.datetime.now().time().strftime("%H:%M:%S") # https://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
 
-def AlarmSound(event):
-    sound = pyglet.resource.media('mlg.mp3')
-    sound.play()
+def alarm_sound(event):
+    pygame.init()
+    pygame.mixer.music.load("mlg.mp3")
+    pygame.mixer.music.play(-1, 0.0)
 
     
 lbl = Label(root, text = " ", width=30,height=2, bg = "black", fg = "white")
@@ -18,13 +19,13 @@ btn = Button(root,                  #родительское окно
              text="Click me",       #надпись на кнопке
              width=30,height=5,     #ширина и высота
              bg="white",fg="black") #цвет фона и надписи
-btn.bind("<Button-1>", Time)       #при нажатии ЛКМ на кнопку вызывается функция Time
+btn.bind("<Button-1>", time)       #при нажатии ЛКМ на кнопку вызывается функция Time
 btn.pack()                          #расположить кнопку на главном окне
 btn1 = Button(root,                 
              text="Click for MLG",       
              width=30,height=5,    
              bg="white",fg="black") 
-btn1.bind("<Button-1>", AlarmSound)       
+btn1.bind("<Button-1>", alarm_sound)       
 btn1.pack()  
 
 root.mainloop()
